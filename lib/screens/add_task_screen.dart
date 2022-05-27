f12db45d2e4a1e28cfbef45279be83526cad6a2f
doanxuanduong <doanxuanduong281296@gmail.com>
2022-05-27 16:07:15 +0700

    duongdxCode

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -8,51 +8,55 @@ class AddTaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     late String newTaskTitle;
 
-    return Container(
-      color: Color(0xff757575),
-      child: Container(
-        padding: EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'Add Task',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30.0,
-                color: Colors.lightBlueAccent,
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return Container(
+          color: const Color(0xff757575),
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
               ),
             ),
-            TextField(
-              autofocus: true,
-              textAlign: TextAlign.center,
-              onChanged: (newText) {
-                newTaskTitle = newText;
-              },
-            ),
-            FlatButton(
-              child: Text(
-                'Add',
-                style: TextStyle(
-                  color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Text(
+                  'Add Task',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
                 ),
-              ),
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                Provider.of<TaskData>(context).addTask(newTaskTitle);
-                Navigator.pop(context);
-              },
+                TextField(
+                  autofocus: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (newText) {
+                    newTaskTitle = newText;
+                  },
+                ),
+                FlatButton(
+                  child: const Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.lightBlueAccent,
+                  onPressed: () {
+                    taskData.addTask(newTaskTitle);
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
